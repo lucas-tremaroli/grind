090a0355da40ab7fcd4155be1e3f1f813689987d
lucas-tremaroli <lucasluigitremaroli@outlook.com>
2025-10-15 01:51:06 +0200

    chore(task): set description height to 5

--- internal/task/form.go
+++ internal/task/form.go
@@ -32,6 +32,7 @@ func NewForm(title, description string, board *Board) *Form {
 
 	form.title.Placeholder = title
 	form.description.Placeholder = description
+	form.description.SetHeight(5)
 	form.title.SetValue(title)
 	form.description.SetValue(description)
 	form.title.Focus()
